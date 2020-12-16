07f87595e33e20632366ef508b51c2720aed71a4
sourodipta <you@example.com>
2020-12-16 16:26:14 +0530

    update

--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
@@ -52,6 +52,38 @@ trait AuthenticatesUsers
 
         return $this->sendFailedLoginResponse($request);
     }
+        /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function superadminlogin(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLoginSuperAdmin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
 
     /**
      * Validate the user login request.
@@ -83,7 +115,22 @@ trait AuthenticatesUsers
             $data, $request->filled('remember')
         );
     }
+  /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLoginSuperAdmin(Request $request)
+    {
 
+      
+        $data=  $this->credentials($request);
+        $data['is_supersdmin']=1;
+        return $this->guard()->attempt(
+            $data, $request->filled('remember')
+        );
+    }
     /**
      * Get the needed authorization credentials from the request.
      *
