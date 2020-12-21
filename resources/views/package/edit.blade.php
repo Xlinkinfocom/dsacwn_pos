1a99e69bf9365066006d35eb28bede668abbd9b7
sourodipta <you@example.com>
2020-12-21 20:38:15 +0530

    update

--- resources/views/package/edit.blade.php
+++ resources/views/package/edit.blade.php
@@ -1,8 +1,8 @@
-@extends('admin.layout.base')
+@extends('layout.main') @section('content')
 
-@section('title', 'Update Credit Package ')
-
-@section('content')
+@if(session()->has('not_permitted'))
+  <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('not_permitted') }}</div> 
+@endif
 
 <div class="content-area py-1">
     <div class="container-fluid">
