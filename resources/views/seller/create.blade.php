1aa97b93659d3121f9a1a71b6cfeee4eea253eec
saumen <info@xlinkinfocom.com>
2020-12-26 22:56:39 +0530

    new changes

--- resources/views/seller/create.blade.php
+++ resources/views/seller/create.blade.php
@@ -374,6 +374,7 @@
                $('#bstate option:selected').text($('#state option:selected').text());
                $('#bdistrict option:selected').text($('#district option:selected').text());
                $('.selectpicker').selectpicker('refresh');
+               $('#bzipcode')val($('#zipcode')val());
            }
        });
 
