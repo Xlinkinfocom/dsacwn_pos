0ac013e09f1353d4f07202fce808d9c6d42da541
sourodipta <you@example.com>
2020-12-28 23:14:32 +0530

    update

--- app/Http/Controllers/ManagecommissionController.php
+++ app/Http/Controllers/ManagecommissionController.php
@@ -32,7 +32,7 @@ class ManagecommissionController extends Controller
             }
             
         }
-        print_r($CommissionMst); exit;
+      //  print_r($CommissionMst); exit;
         return view('managecommission.index', compact('CommissionMst'));
     }
 
