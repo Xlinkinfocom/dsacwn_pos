5f6c0e5d5e0b40ed0c1ea209d6f18ecd60936c1f
sourodipta <you@example.com>
2021-01-01 11:47:43 +0530

    update

--- resources/views/sellerpackage/add.blade.php
+++ resources/views/sellerpackage/add.blade.php
@@ -21,18 +21,21 @@
                                     <h4 class="card-title">Card title</h4>
                                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                   </div>
+                                  <a href="#!" class="btn btn-primary">Go somewhere</a>
                                 </div>
                                 <div class="card">
                                   <div class="card-body">
                                     <h4 class="card-title">Card title</h4>
                                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                                   </div>
+                                  <a href="#!" class="btn btn-primary">Go somewhere</a>
                                 </div>
                                 <div class="card">
                                   <div class="card-body">
                                     <h4 class="card-title">Card title</h4>
                                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                                   </div>
+                                  <a href="#!" class="btn btn-primary">Go somewhere</a>
                                 </div>
                               </div>
                         </form>
