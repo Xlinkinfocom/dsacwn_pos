4df999e1d48ee7e4513a37fcbd07b7d0e22c5ace
sourodipta <you@example.com>
2021-01-01 11:48:55 +0530

    update

--- resources/views/sellerpackage/add.blade.php
+++ resources/views/sellerpackage/add.blade.php
@@ -20,22 +20,25 @@
                                   <div class="card-body">
                                     <h4 class="card-title">Card title</h4>
                                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <a href="#!" class="btn btn-primary">Go somewhere</a>
                                   </div>
-                                  <a href="#!" class="btn btn-primary">Go somewhere</a>
+                                  
                                 </div>
                                 <div class="card">
                                   <div class="card-body">
                                     <h4 class="card-title">Card title</h4>
                                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                    <a href="#!" class="btn btn-primary">Go somewhere</a>
                                   </div>
-                                  <a href="#!" class="btn btn-primary">Go somewhere</a>
+                                  
                                 </div>
                                 <div class="card">
                                   <div class="card-body">
                                     <h4 class="card-title">Card title</h4>
                                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                    <a href="#!" class="btn btn-primary">Go somewhere</a>
                                   </div>
-                                  <a href="#!" class="btn btn-primary">Go somewhere</a>
+                                
                                 </div>
                               </div>
                         </form>
