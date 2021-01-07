0ab7e0f44f5f13724a913602cdd31cd9e3d3e53b
saumen <info@xlinkinfocom.com>
2021-01-07 17:00:04 +0530

    new changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -320,7 +320,7 @@ class ProductController extends Controller
                 ->get();
             }
 
-            dd($seller_list);
+            
            
             return view('product.create',compact('seller_list', 'lims_product_list', 'lims_brand_list', 'lims_category_list', 'lims_unit_list', 'lims_tax_list'));
         }
