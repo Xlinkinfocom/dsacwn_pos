6f2569c7aecd0f5b18897f69731e185243a4df5a
saumen <info@xlinkinfocom.com>
2021-01-09 23:14:50 +0530

    new changes

---
+++ app/Stransaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stransaction extends Model
+{
+    public $timestamps = true;
+
+    protected $attributes = [
+        'seller_id'                 => true, 
+        'invoice_id'                => true,
+        'invoice_date'              => true,
+        'sale_value'                => true,
+        'seller_payable_amount'    => true,       
+    ];
+
+    public function seller()
+    {
+    	return $this->belongsTo('App\Seller', 'seller_id')->select(['id', 'seller_name']);
+    }
+
+    public function sale()
+    {
+        return $this->belongsTo('App\Sale', 'invoice_id');
+    }
+   
+}
