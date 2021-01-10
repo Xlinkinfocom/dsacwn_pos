90352ed47ff6bfe3a3d8e504f6ef0f70feacc198
saumen <info@xlinkinfocom.com>
2021-01-10 15:01:57 +0530

    new changes

--- app/Http/Controllers/StransactionController.php
+++ app/Http/Controllers/StransactionController.php
@@ -93,13 +93,13 @@ class StransactionController extends Controller
                                         }
                                         else
                                         {
-                                            echo $product->category_id;
-                                            /* $get_commission = CommissionMst::select('total_commissoin')
+                                            $get_commission = DB::table('commission_mst')
+                                                            ->select('total_commissoin')
                                                             ->where('cat_id', $product->category_id)
                                                             ->orWhere('sub_cat_id', $product->category_id)
                                                             ->get();
                                             echo '<pre>';
-                                            print_r($get_commission); */
+                                            print_r($get_commission);
                                         }
                                     }
                                 }
