7a18bd5f86b3e5633583ef0ae9c962acd797fe88
saumen <info@xlinkinfocom.com>
2021-01-10 23:17:19 +0530

    new changes

--- resources/views/stransaction/index.blade.php
+++ resources/views/stransaction/index.blade.php
@@ -3,6 +3,53 @@
 @php 
 if(!empty($transactions))
 {
+    @endphp
+    <section class="forms">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-header mt-2">
+                <h3 class="text-center">{{trans('file.Sale Report')}}</h3>
+            </div>
+            {!! Form::open(['route' => 'report.sale', 'method' => 'post']) !!}
+            <div class="row mb-3">
+                <div class="col-md-4 offset-md-1 mt-4">
+                    <div class="form-group row">
+                        <label class="d-tc mt-2"><strong>{{trans('file.Choose Your Date')}}</strong> &nbsp;</label>
+                        <div class="d-tc">
+                            <div class="input-group">
+                                <input type="text" class="daterangepicker-field form-control" value="{{$start_date}} To {{$end_date}}" required />
+                                <input type="hidden" name="start_date" value="{{$start_date}}" />
+                                <input type="hidden" name="end_date" value="{{$end_date}}" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-4">
+                    <div class="form-group row">
+                        <label class="d-tc mt-2"><strong>{{trans('file.Choose Seller')}}</strong> &nbsp;</label>
+                        <div class="d-tc">
+                            <input type="hidden" name="warehouse_id_hidden" value="" />
+                            <select id="seller_id" name="warehouse_id" class="selectpicker form-control" data-live-search="true" data-live-search-style="begins" >
+                                <option value="0">{{trans('file.All Seller')}}</option>
+                                @foreach($sellers as $seller)
+                                <option value="{{$seller->id}}">{{$seller->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mt-4">
+                    <div class="form-group">
+                        <button class="btn btn-primary" type="submit">{{trans('file.submit')}}</button>
+                    </div>
+                </div>
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+    
+</section>
+    @php
 
 }
 else {
