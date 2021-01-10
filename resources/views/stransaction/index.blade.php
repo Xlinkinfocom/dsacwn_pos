9731a256f90809c637dab3575948e528aefe17b1
saumen <info@xlinkinfocom.com>
2021-01-10 23:12:39 +0530

    new changes

--- resources/views/stransaction/index.blade.php
+++ resources/views/stransaction/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layout.main') @section('content')
 
 @php 
-print_r($transactions); 
-
-foreach($transactions as $transaction)
+if(!empty($transactions))
 {
-    echo $transaction['seller_name'];
-}
 
+}
+else {
+@endphp
+    <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{'No Data exist between this date range!'}}</div>
+@php
+}
 @endphp
 
 @endsection
