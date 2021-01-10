597b9feeeeeeda75a6fc601109d58eeb7a82615a
saumen <info@xlinkinfocom.com>
2021-01-11 01:22:07 +0530

    new changes

--- app/Http/Controllers/StransactionController.php
+++ app/Http/Controllers/StransactionController.php
@@ -151,7 +151,7 @@ class StransactionController extends Controller
             }
             else
             {
-                dd();
+                dd(Auth::user()->id);
                 $sellers = User::select('id', 'name')
                                 ->where('role_id', '7')
                                 ->where('id', Auth::user()->id)
