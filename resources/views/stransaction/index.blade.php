4b8cd38f38d3669ae3f05ec45674fe169b2eb999
saumen <info@xlinkinfocom.com>
2021-01-24 19:29:34 +0530

    new changes

--- resources/views/stransaction/index.blade.php
+++ resources/views/stransaction/index.blade.php
@@ -32,10 +32,8 @@
                         <div class="d-tc">
                             <div class="input-group">
                                 <input name="start_date" type="date" class="form-control" value="" required />
-                            </div>
-                            <div class="input-group">
                                 <input name="end_date" type="date" class="form-control" value="" required />
-                            </div>
+                            </div>                            
                         </div>                       
                     </div>
                 </div>                               
