ef40a7d8ca8f501b6ebebc761e76d9cef0ed2e56
saumen <info@xlinkinfocom.com>
2021-01-29 17:11:26 +0530

    n c

--- app/Http/Controllers/StransactionController.php
+++ app/Http/Controllers/StransactionController.php
@@ -549,7 +549,7 @@ class StransactionController extends Controller
 
                             } else {
 
-                                $payments = DB::table('payments')
+                            $payments = DB::table('payments')
                             ->join('sales', 'payments.sale_id', '=', 'sales.id')
                             ->select('sales.reference_no', 'payments.sale_id', 'payments.amount', 'payments.by_cash', 'payments.by_card', 'payments.paying_method', 'payments.created_at')
                             ->where('payments.user_id', $seller->id)                            
